fdc64ac4416301d0b4e938c02556d29ae087c6fc
Abdoul Gadiry Barry <abdoulgadirybarry01@gmail.com>
2023-07-31 17:30:43 +0000

    Mise en place de la bannière et du footer

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './styles/home.css';
+import "./styles/app.css";
+import "./styles/home.css";
 
 // start the Stimulus application
-import './bootstrap';
-import './js/nav';
+import "./bootstrap";
+import "./js/nav";
